4d5bcdaa5f2f17250a0a6f5845e70b40b91d2fcc
Huy Tran <tunhuytrn@gmail.com>
2023-10-23 12:31:23 -0400

    feat: moved sensitive data to enviroment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'https://task-tracking-server-feabfeb69418.herokuapp.com/task'
+    serverUrl: 'https://task-tracking-server-feabfeb69418.herokuapp.com'
 };
