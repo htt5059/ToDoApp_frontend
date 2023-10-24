60c3149811fdb50db1c59ca419ebb9877ed2bae5
htt5059 <74249592+htt5059@users.noreply.github.com>
2023-10-24 16:04:37 -0400

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    serverUrl: 'http://localhost:3000',
-    FacebookClientId: '723613323145449',
-    GoogleClientId: '77826641125-729trg3n8ndmo09vnv2v4unebo77prhd.apps.googleusercontent.com'
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    serverUrl: 'https://task-tracking-server-feabfeb69418.herokuapp.com',
-    FacebookClientId: '723613323145449',
-    GoogleClientId: '77826641125-729trg3n8ndmo09vnv2v4unebo77prhd.apps.googleusercontent.com'
-};
